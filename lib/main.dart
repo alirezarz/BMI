72c07969b30c9859ed9ec016eb3bf4e46c90fc24
alirezarz <alireza_knut@yahoo.com>
2022-07-07 19:02:03 +0430

    add Appbar and TextFeilds

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,49 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.black,
       ),
       body: Column(
-        
+        children: [
+          const SizedBox(
+            height: 15.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.black,
+                  height: 90.0,
+                  width: 130.0,
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Height',
+                      hintStyle: TextStyle(
+                        fontSize: 35.0,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.black,
+                  height: 90.0,
+                  width: 130.0,
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Weight',
+                      hintStyle: TextStyle(
+                        fontSize: 35.0,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
